ef03210416910737f8bcd3815a70e6631a649bb6
prashantsnehi <prashant.snehi@gmail.com>
2023-07-03 18:31:25 +0530

    Object Inheritance in deep

--- learning/16_object_inheritance.py
+++ learning/16_object_inheritance.py
@@ -62,7 +62,55 @@ class Cow(Animal):
 hamlet = Piglet("Hamlet")
 hamlet.speak()
 
-
 """Creating object of Cow class"""
 milky = Cow("Milky-White")
 milky.speak()
+
+"""Let's expand a bit on our clothing class. Finish the 'Stock_by_Material' method and iterate over the amount of each item of a given material that is in stock. When you are finidshed 
+the script should add up to 10 cotton clothing items"""
+
+
+class Clothing:
+    stock = {'name': [], 'material': [], 'amount': []}
+
+    def __init__(self, name):
+        material = ""
+        self.name = name
+
+    def add_item(self, name, material, amount):
+        Clothing.stock['name'].append(self.name)
+        Clothing.stock['material'].append(self.material)
+        Clothing.stock['amount'].append(amount)
+
+    def stock_by_material(self, material):
+        """
+        :param material:
+        :return:
+        """
+        count = 0
+        n = 0
+        for item in Clothing.stock['material']:
+            if item == material:
+                count += Clothing.stock['amount'][n]
+                n += 1
+        return count
+
+
+class Shirt(Clothing):
+    material = "Cotton"
+class Pants(Clothing):
+    material = "Cotton"
+
+polo = Shirt("Polo")
+sweatpants = Pants("Sweatpants")
+
+polo.add_item(polo.name,polo.material,4)
+sweatpants.add_item(sweatpants.name,sweatpants.material,6)
+
+current_stock = polo.stock_by_material("Cotton")
+print(current_stock)
+
+
+
+
+
