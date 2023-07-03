f5e511eba4e35ea1ac5de58e79141508c7dd4973
prashantsnehi <prashant.snehi@gmail.com>
2023-07-03 16:36:15 +0530

    Inheritance

--- learning/16_object_inheritance.py
+++ learning/16_object_inheritance.py
@@ -27,3 +27,31 @@ carnelian = Grapes('purple', 'sweet')
 
 print("Grenny Smith apples are {} and {}".format(granny_smith.color, granny_smith.flavor))
 print("Carnelian grapes are {} and {}".format(carnelian.color, carnelian.flavor))
+
+
+class Animal:
+    sound = ""
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        print("This is Animal Class")
+
+    def speak(self):
+        print("{sound} I'm {name}! {sound}".format(sound=self.sound, name=self.name))
+
+
+class Piglet(Animal):
+    sound = "Oink!"
+
+
+class Cow(Animal):
+    sound = "Moooooo!"
+
+
+hamlet = Piglet("Hamlet")
+hamlet.speak()
+
+milky = Cow("Milky-White")
+milky.speak()
