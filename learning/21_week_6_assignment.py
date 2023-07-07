7e023d4481eeb13a433dfc964e81aef15d01e837
prashantsnehi <prashant.snehi@gmail.com>
2023-07-07 18:54:12 +0530

    C1M5_Object_Oriented_Programming_V8

--- learning/21_week_6_assignment.py
+++ learning/21_week_6_assignment.py
@@ -7,22 +7,21 @@ def current_user(events):
     machines = {}
     for event in events:
         if event.machine not in machines:
-            machines[event.machine] = set()
+            # machines[event.machine] = set()
+            machines[event.machine] = []
         if event.type == "login":
-            machines[event.machine].add(event.user)
-        elif event.type == "logout":
+            # machines[event.machine].add(event.user)
+            machines[event.machine].append(event.user)
+        elif event.type == "logout" and event.user in machines[event.machine]:
             machines[event.machine].remove(event.user)
     return machines
 
 
 def generate_report(machines):
     for machine_id, curr_user in machines.items():
-        print('user: ' + str(curr_user) + ' ' + str(len(curr_user)))
+        # print('user: ' + str(curr_user) + ' ' + str(len(curr_user)))
         if len(curr_user) > 0:
-            user_list = ",".join(curr_user)
-        print("{}: {}".format(machine_id, user_list))
-
-
+            print("{}: {}".format(machine_id, ", ".join(curr_user)))
 
 
 class Event:
@@ -39,11 +38,13 @@ events = [
     Event('2020-01-21 18:53:21', 'login', 'webserver.local', 'lane'),
     Event('2020-01-22 10:25:34', 'logout', 'myworkstation.local', 'jordan'),
     Event('2020-01-21 08:20:01', 'login', 'webserver.local', 'jordan'),
-    Event('2020-01-23 11:24:42', 'login', 'mailserver.local', 'chris')
+    Event('2020-01-23 11:24:42', 'login', 'mailserver.local', 'chris'),
+    Event('2020-01-23 11:24:42', 'login', 'mailserver.local', 'kuldeep'),
+    Event('2020-01-21 08:20:01', 'login', 'webserver.local', 'sushil'),
+    Event('2020-01-21 08:20:01', 'logout', 'webserver.local', 'tusshar')
 ]
 
 users = current_user(events)
 print(users)
 
 generate_report(users)
-
